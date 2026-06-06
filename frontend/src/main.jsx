818cf471090a044df20a0002fc7b5d4ff280c5a8
Rohan Sai <rohansai132005@gmail.com>
2026-06-06 08:36:39 +0530

    V.18.3.11

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
+// import { SpeedInsights } from "@vercel/speed-insights/react"
 import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.jsx'
